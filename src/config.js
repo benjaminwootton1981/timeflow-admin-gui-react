78bae2616aba3448dfb757495947fc874671acc1
cleanDev-win <beautifulsky1991@hotmail.com>
2020-06-12 16:05:43 +0300

    update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://52.215.11.107/api/v1/";
+export const API_URL = "http://52.213.90.209/api/v1/";
 export const DEVELOPER_KEY =
-  "0d943f5a09b761db8ccaa813d3adf5596102f5d1ace600b1adf1604ed052ede1";
+  "6ec27af68aa057caf4e39622e16b518a9be2e669a15aca73fd77abd229923dab";
 export const PROJECT_KEY =
-  "c3ae6277285a9c32885c652f04499cb34c8a75c6bfc4824defe2ed8e8d35e4f1";
+  "9ed9facf5789045b5ac56937c65b69c2d3c1c9c350f34d73728161b08aa31370 ";
