0188bef7f16a8ffb1996d3e5d200cd4978d76412
benjamin wootton <benjamin.wootton.personal@gmail.com>
2020-06-12 17:23:22 +0100

    Change api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://52.215.11.107/api/v1/";
+export const API_URL = "http://52.213.90.209/api/v1/";
 export const DEVELOPER_KEY =
-  "0d943f5a09b761db8ccaa813d3adf5596102f5d1ace600b1adf1604ed052ede1";
+  "6ec27af68aa057caf4e39622e16b518a9be2e669a15aca73fd77abd229923dab";
 export const PROJECT_KEY =
-  "c3ae6277285a9c32885c652f04499cb34c8a75c6bfc4824defe2ed8e8d35e4f1";
+  "f5c1df78fbcb4a3427a7145e3d6eea9f145e54ebc3d2730441ce0ea1fa4fc488";
