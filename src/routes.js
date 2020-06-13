f05c7a3bacdbb1d65a906511fda0d8522b227c49
benjamin wootton <benjamin.wootton.personal@gmail.com>
2020-06-13 06:43:23 +0100

    URL routes to receive traffic from django

--- src/routes.js
+++ src/routes.js
@@ -16,8 +16,24 @@ class Routes extends Component {
           <AppLayout>
             <BrowserRouter basename={"/react"}>
               <Switch>
-                <Route exact path='/' component={ManageStreamProcessor} />
-                <Route exact path='/monitor' component={MonitorStreamProcessor} />
+                {/* <Route exact path='/' component={ManageStreamProcessor} />
+                <Route exact path='/monitor' component={MonitorStreamProcessor} /> */}
+
+                # Access From Django - Streams Index Page - /react/projects/9/streams/
+                <Route exact path='/projects/9/streams/' component={ManageStreamProcessor} />
+
+                # Access From Django - Simulations Index Page - /react/projects/9/simulations/
+                <Route exact path='/projects/9/simulations/' component={ManageStreamProcessor} />
+
+                # Access From Django - Stream Processors Index Page - /react/projects/9/streamprocessors/
+                <Route exact path='/projects/9/streamprocessors/' component={ManageStreamProcessor} />
+
+                # Access From React - Stream Processors Monitor Page 
+                <Route exact path='/projects/9/streamprocessors/1/monitor' component={MonitorStreamProcessor} />
+
+                # Access From React - Stream Processors New/Edit Page
+                {/* <Route exact path='/projects/9/streamprocessors/new' component={NewMonitorStreamProcessor} /> */}
+
               </Switch>
             </BrowserRouter>
           </AppLayout>
