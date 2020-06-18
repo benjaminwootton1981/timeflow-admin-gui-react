6fb10630d422870795db7bf7f3b6c51a54528c29
Victor A <victor@namespace.ee>
2020-06-19 02:46:43 +0300

    get stream events count

--- src/components/cards/StreamValueCard.js
+++ src/components/cards/StreamValueCard.js
@@ -1,58 +1,87 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../config";
 
-class StreamValueCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const StreamValueCard = ({ item }) => {
+  const [events, setEvents] = useState(0);
 
-  render() {
-    const item = this.props.post;
-    return (
-      <div className="Valuecard">
-        <h2 className="valueHeader">
-          {item.display_name}
-        </h2>
-        <div className="cardBody">
-          <div className="grid">
-            <div className="cardItem smallItem">
-              <span className="cardInput" data-header="Number Of Events">2</span>
-            </div>
-            <div className="cardItem smallItem">
-              <span className="cardInput" data-header="Number Of Errors">1</span>
-            </div>
+  useEffect(() => {
+    if (item.id) {
+      axios
+        .get(`${API_URL}stream_events`, { params: { stream: item.id } })
+        .then((response) => {
+          setEvents(response.data.eventCount);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  }, [item.id]);
+
+  return (
+    <div className="Valuecard">
+      <h2 className="valueHeader">{item.display_name}</h2>
+      <div className="cardBody">
+        <div className="grid">
+          <div className="cardItem smallItem">
+            <span className="cardInput" data-header="Number Of Events">
+              {events}
+            </span>
           </div>
-          <span className="cardInput greenOutline" data-header="Status">Healthy</span>
+          <div className="cardItem smallItem">
+            <span className="cardInput" data-header="Number Of Errors">
+              0
+            </span>
+          </div>
         </div>
-        <div className="cardFooter">
-          <a href={`/projects/${item.project && item.project.id}/streams/${item.id}/edit/`} className='edit'>
-            <span className="helper">Edit</span>
-          </a>
-          <a href={`/projects/${item.project && item.project.id}/streams/${item.id}/monitor`} className='monitor'>
-            <span className="helper">Monitor</span>
-          </a>
-          <a href={`/projects/${item.project && item.project.id}/streams/${item.id}/analyse`} className='export'>
-            <span className="helper">Analyse</span>
-          </a>
-          <a href={`/projects/${item.project && item.project.id}/streams/${item.id}/reset`} className='reset'>
-            <span className="helper">Reset</span>
-          </a>
-          <a href={`/projects/${item.project && item.project.id}/streams/${item.id}/delete/`} className='delete'>
-            <span className="helper">Delete</span>
-          </a>
-        </div>
+        <span className="cardInput greenOutline" data-header="Status">
+          Healthy
+        </span>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {};
+      <div className="cardFooter">
+        <a
+          href={`/projects/${item.project && item.project.id}/streams/${
+            item.id
+          }/edit/`}
+          className="edit"
+        >
+          <span className="helper">Edit</span>
+        </a>
+        <a
+          href={`/projects/${item.project && item.project.id}/streams/${
+            item.id
+          }/monitor`}
+          className="monitor"
+        >
+          <span className="helper">Monitor</span>
+        </a>
+        <a
+          href={`/projects/${item.project && item.project.id}/streams/${
+            item.id
+          }/analyse`}
+          className="export"
+        >
+          <span className="helper">Analyse</span>
+        </a>
+        <a
+          href={`/projects/${item.project && item.project.id}/streams/${
+            item.id
+          }/reset`}
+          className="reset"
+        >
+          <span className="helper">Reset</span>
+        </a>
+        <a
+          href={`/projects/${item.project && item.project.id}/streams/${
+            item.id
+          }/delete/`}
+          className="delete"
+        >
+          <span className="helper">Delete</span>
+        </a>
+      </div>
+    </div>
+  );
 };
 
-const mapDispatchToProps = dispatch => {
-  return {};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(StreamValueCard);
+export default StreamValueCard;
