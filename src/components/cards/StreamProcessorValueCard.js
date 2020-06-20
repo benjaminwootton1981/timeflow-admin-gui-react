c13acb1857fa44ffed9906f1d8906f6a9ff0d95e
Victor A <victor@namespace.ee>
2020-06-20 17:54:26 +0300

    trigger run from react

--- src/components/cards/StreamProcessorValueCard.js
+++ src/components/cards/StreamProcessorValueCard.js
@@ -1,95 +1,93 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import ReplicaInfo from "../managestream/ReplicaInfo";
+import axios from "axios";
+import { API_URL } from "../../config";
+import { notification } from "antd";
 
-class StreamProcessorValueCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const StreamProcessorValueCard = ({ post: item }) => {
+  const handleRun = () => {
+    axios
+      .post(`${API_URL}streamprocessor_action/run`, {
+        project_id: item.project && item.project.id,
+        streamprocessor_id: item.id,
+      })
+      .then((response) => {
+        const status = response.data.status;
 
-  render() {
-    const item = this.props.post;
-    return (
-      <div className="Valuecard">
-        <h2 className="valueHeader">{item.name}</h2>
-        <ReplicaInfo
-          eventId={item.id}
-          projectId={item.project && item.project.id}
-          userId={item.owning_user && item.owning_user.id}
-          requestedReplicas={item.replicas}
-          eventType={"streamprocessor"}
-        />
-        <div className="cardFooter">
-          <a
-            href={`/projects/${
-              item.project && item.project.id
-            }/streamprocessors/${item.id}/edit/`}
-            className="edit"
-          >
-            {/*EDIT - Needs to go to - /projects/2/streamprocessors/3/edit/ -->*/}
-            <span className="helper">Edit</span>
-          </a>
-          <a
-            href={`/projects/${
-              item.project && item.project.id
-            }/streamprocessors/${item.id}/duplicate/`}
-            className="duplicate"
-          >
-            {/*DUPLICATE -  Needs to go to - /projects/2/streamprocessors/3/duplicate/-->*/}
-            <span className="helper">Duplicate</span>
-          </a>
-          <a
-            href={`/projects/${
-              item.project && item.project && item.project.id
-            }/streamprocessors/${item.id}/run/`}
-            className="deploy"
-          >
-            {/*DEPLOY - Needs to go to - /projects/2/streamprocessors/3/run/ -->*/}
-            <span className="helper">Deploy</span>
-          </a>
-          <a
-            href={`/projects/${
-              item.project && item.project.id
-            }/streamprocessors/${item.id}/stop/`}
-            className="pause"
-          >
-            {/*STOP - Needs to go to - /projects/2/streamprocessors/3/stop/ -->*/}
-            <span className="helper">Stop</span>
-          </a>
-          <a
-            href={`/react/projects/${
-              item.project && item.project.id
-            }/streamprocessors/${item.id}/monitor/`}
-            className="monitor"
-          >
-            {/*MONITOR - Needs to go to - /projects/2/streamprocessors/3/monitor/ -->*/}
-            <span className="helper">Monitor</span>
-          </a>
-          <a
-            href={`/projects/${
-              item.project && item.project.id
-            }/streamprocessors/${item.id}/delete/`}
-            className="delete"
-          >
-            {/*DELETE  Needs to go to - /projects/2/streamprocessors/3/delete/ -->*/}
-            <span className="helper">Delete</span>
-          </a>
-        </div>
+        if (status === "success") {
+          notification.success({
+            message: "Stream Processor deployed successfully",
+          });
+        } else {
+          notification.error({
+            message: "Stream Processor deployment failed.",
+          });
+        }
+      });
+  };
+  return (
+    <div className="Valuecard">
+      <h2 className="valueHeader">{item.name}</h2>
+      <ReplicaInfo
+        eventId={item.id}
+        projectId={item.project && item.project.id}
+        userId={item.owning_user && item.owning_user.id}
+        requestedReplicas={item.replicas}
+        eventType={"streamprocessor"}
+      />
+      <div className="cardFooter">
+        <a
+          href={`/projects/${
+            item.project && item.project.id
+          }/streamprocessors/${item.id}/edit/`}
+          className="edit"
+        >
+          {/*EDIT - Needs to go to - /projects/2/streamprocessors/3/edit/ -->*/}
+          <span className="helper">Edit</span>
+        </a>
+        <a
+          href={`/projects/${
+            item.project && item.project.id
+          }/streamprocessors/${item.id}/duplicate/`}
+          className="duplicate"
+        >
+          {/*DUPLICATE -  Needs to go to - /projects/2/streamprocessors/3/duplicate/-->*/}
+          <span className="helper">Duplicate</span>
+        </a>
+        <button onClick={handleRun} className="deploy">
+          {/*DEPLOY - Needs to go to - /projects/2/streamprocessors/3/run/ -->*/}
+          <span className="helper">Deploy</span>
+        </button>
+        <a
+          href={`/projects/${
+            item.project && item.project.id
+          }/streamprocessors/${item.id}/stop/`}
+          className="pause"
+        >
+          {/*STOP - Needs to go to - /projects/2/streamprocessors/3/stop/ -->*/}
+          <span className="helper">Stop</span>
+        </a>
+        <a
+          href={`/react/projects/${
+            item.project && item.project.id
+          }/streamprocessors/${item.id}/monitor/`}
+          className="monitor"
+        >
+          {/*MONITOR - Needs to go to - /projects/2/streamprocessors/3/monitor/ -->*/}
+          <span className="helper">Monitor</span>
+        </a>
+        <a
+          href={`/projects/${
+            item.project && item.project.id
+          }/streamprocessors/${item.id}/delete/`}
+          className="delete"
+        >
+          {/*DELETE  Needs to go to - /projects/2/streamprocessors/3/delete/ -->*/}
+          <span className="helper">Delete</span>
+        </a>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {};
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(StreamProcessorValueCard);
+export default StreamProcessorValueCard;
