6d4cbca9b2752888e5de00bc267b0623c13b09c8
Victor A <victor@namespace.ee>
2020-06-24 17:41:37 +0300

    show all steps

--- src/pages/streamProcessor/monitor/MonitorContent.js
+++ src/pages/streamProcessor/monitor/MonitorContent.js
@@ -1,52 +1,42 @@
-import React, { Component } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import MonitorChildCard from "../../../components/cards/MonitorChildCard";
 import StepConnectorSVG from "../../../assets/step_connector.svg";
+import api from "../../../api";
 
 export default function MonitorContent({ projectId, streamProcessorId }) {
+  const [steps, setSteps] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("streamprocessorstep/step-types")
+      .then((response) => {
+        setSteps(response.data.step_types);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   return (
     <div className="monitor__body">
-      <MonitorChildCard
-        parent="inbound"
-        title="Inbound Event"
-        projectId={projectId}
-        streamProcessorId={streamProcessorId}
-      />
-      <div className="monitor__body_child_linter">
-        <img src={StepConnectorSVG} alt="step connector" />
-      </div>
-      <MonitorChildCard
-        parent="simple"
-        title="Simple Filter"
-        projectId={projectId}
-        streamProcessorId={streamProcessorId}
-      />
-      <div className="monitor__body_child_linter">
-        <img src={StepConnectorSVG} alt="step connector" />
-      </div>
-      <MonitorChildCard
-        parent="stream"
-        title="Stream Lookup"
-        projectId={projectId}
-        streamProcessorId={streamProcessorId}
-      />
-      <div className="monitor__body_child_linter">
-        <img src={StepConnectorSVG} alt="step connector" />
-      </div>
-      <MonitorChildCard
-        parent="map"
-        title="Map to Event Type"
-        projectId={projectId}
-        streamProcessorId={streamProcessorId}
-      />
-      <div className="monitor__body_child_linter">
-        <img src={StepConnectorSVG} alt="step connector" />
-      </div>
-      <MonitorChildCard
-        parent="outbound"
-        title="Outbound Step"
-        projectId={projectId}
-        streamProcessorId={streamProcessorId}
-      />
+      {steps.map((step, index) => {
+        return (
+          <Fragment key={index}>
+            <MonitorChildCard
+              key={step.value}
+              parent={step.value}
+              title={step.name}
+              projectId={projectId}
+              streamProcessorId={streamProcessorId}
+            />
+            {index !== steps.length - 1 && (
+              <div className="monitor__body_child_linter">
+                <img src={StepConnectorSVG} alt="step connector" />
+              </div>
+            )}
+          </Fragment>
+        );
+      })}
     </div>
   );
 }
